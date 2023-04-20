1a022f184f9729737dea53ebc056ca1deb36e5e9
prateekchhibber <0392pc@gmail.com>
2023-04-20 20:22:04 +0530

    Added NavBurger to right side

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 function header() {
   return (
@@ -17,10 +18,19 @@ function header() {
       <RightMenu>
       <a href="#">Shop</a>
       <a href="#">Tesla Account</a>
-      <CustomMenu>
-
-      </CustomMenu>
+      <CustomMenu />
       </RightMenu>
+      <BurgerNav>
+        <CloseWrapper>
+        <CustomClose />
+        </CloseWrapper>
+        <li><a href="#">Existing Inventory</a></li>
+        <li><a href="#">Used Inventory</a></li>
+        <li><a href="#">Trade-In</a></li>
+        <li><a href="#">Cybertruck</a></li>
+        <li><a href="#">Roadster</a></li>
+        <li><a href="#">Existing Inventory</a></li>
+      </BurgerNav>
     </Container>
   );
 }
@@ -32,10 +42,12 @@ const Container = styled.div`
   position: fixed;
   display: flex;
   align-items: center;
+  justify-content: space-between;
   padding: 0 20px;
   top: 0;
   left: 0;
   right: 0;
+  z-index: 1;
 `;
 const Menu = styled.div`
   display: flex;
@@ -64,4 +76,32 @@ a {
 `
 const CustomMenu = styled(MenuIcon)`
   cursor: pointer;
+`
+const BurgerNav = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background: white;
+  width: 300px;
+  z-index: 16;
+  list-style: none;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  li {
+      padding: 15px 0;
+      border-bottom: 1px solid rgba(0,0,0, .2);
+      a {
+        font-weight: 600;
+      }
+  }
+`
+const CustomClose = styled(CloseIcon)`
+  cursor: pointer;
+`
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
 `
